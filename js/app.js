e269df085f14d38c06212bd41baa2ce75b914586
Mattdre <olsen_matthew@yahoo.com>
2016-04-24 13:33:15 -1000

    update

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ var challenges = [{
 },{
 	dailyChallenge: 'Use public transportation instead of driving today',
 	rating: 2,
-	benefits: '',
+	benefits: 'Create only a fraction of the pollution generated from driving also save roughly $9,000 annually ',
 },{
 	dailyChallenge: 'Plant a tree!',
 	rating: 3,
-	benefits: '',
+	benefits: 'To make up for the loss of trees over the past decade, 130 million hectares (or 1.3 million square kilometers), an area as large as Peru, would have to be reforested. Accomplishing that would mean planting about 14 billion trees every year for 10 years in a row, the equivalent of every person on Earth planting and caring for at least two seedlings annually.',
 },{
 	dailyChallenge: 'Buy only local produce',
 	rating: 1,
@@ -17,7 +17,7 @@ var challenges = [{
 },{
 	dailyChallenge: 'Use reusable shopping bags today',
 	rating: 1,
-	benefits: '',
+	benefits: 'Do you part in reducing the average of 22,000 plastic bags used by each person in their lifetime ',
 },{
 	dailyChallenge: 'Carpool',
 	rating: 2,
@@ -42,6 +42,10 @@ var challenges = [{
 	dailyChallenge: 'Carry a water bottle with you',
 	rating: 1,
 	benefits: '',
+},{
+	dailyChallenge: 'Pick up 3 pieces of trash today',
+	rating: 1,
+	benefits: 'Ten million people. That’s 30 million pieces of trash per day.  What if each person only picked that piece of trash every other day?  That is 30 million people each picking up 182.5 pieces of trash per year, that’s almost 5.5 billion pieces of trash picked up.  That is pretty powerful, billions of pieces of trash less going into our waterways and oceans.',
 }];
 
 function randomChallenge(things){
