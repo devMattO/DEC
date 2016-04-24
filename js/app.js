081ebbd07137fb1a3cb106a7b9490c308c77dd26
Mattdre <olsen_matthew@yahoo.com>
2016-04-23 16:02:12 -1000

    initial challenges array

---
+++ js/app.js
@@ -0,0 +1,48 @@
+var challenges = [{
+	dailyChallenge: 'Take a shower in under 5 minutes',
+	rating: 1,
+	benefits: 'Save 6,000 gallons of water per year!',
+},{
+	dailyChallenge: 'Use public transportation instead of driving today',
+	rating: 2,
+	benefits: '',
+},{
+	dailyChallenge: 'Plant a tree!',
+	rating: 3,
+	benefits: '',
+},{
+	dailyChallenge: 'Buy only local produce',
+	rating: 1,
+	benefits: '',
+},{
+	dailyChallenge: 'Use reusable shopping bags today',
+	rating: 1,
+	benefits: '',
+},{
+	dailyChallenge: 'Carpool',
+	rating: 2,
+	benefits: 'Thanks for doing your part in not contribiting to the 60 billion dollar loss of gas and time caused by solo commuters',
+},{
+	dailyChallenge: 'Unplug all idle kitchen appliances',
+	rating: 1,
+	benefits: '',
+},{
+	dailyChallenge: 'Laundry day... Air dry your clothes!',
+	rating: 2,
+	benefits: '',
+},{
+	dailyChallenge: 'Go to optoutprescreen.com to stop receiving pre-approved credit card offers and sign up on catalogchoice.org to reduce the amount of unsolicited catalogs sent to you.',
+	rating: 2,
+	benefits: '',
+},{
+	dailyChallenge: 'Donate old tech/cellphones',
+	rating: 1,
+	benefits: '',
+},{
+	dailyChallenge: 'Carry a water bottle with you',
+	rating: 1,
+	benefits: '',
+}];
+
+
+
